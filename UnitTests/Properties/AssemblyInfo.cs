4fdd0e09e9f35ea0445ee5859b76074e50fab1bf
Ivan Bitunjac <ivbitunj@vsite.hr>
2021-11-08 18:38:14 +0100

    Add project files.

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e1b0088e-f8fb-41a7-82e5-72c026b64ed4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Factorial.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FactorialReturns1ForArgument0()
+        {   
+            var result = Math.Factorial(0);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void FactorialReturns1ForArgument1()
+        {
+            var result = Math.Factorial(1);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void FactorialReturns2ForArgument2()
+        {
+            var result = Math.Factorial(2);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
